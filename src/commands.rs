32c8fd4389acb58cec979f13d0755e002c1aaaba
masuke5 <s.zerogoichi@gmail.com>
2018-08-15 14:43:45 +0900

    Move reading and parsing page file to a get_page_by_id

--- src/commands.rs
+++ src/commands.rs
@@ -11,6 +11,31 @@ const PAGE_EXTENSION: &str = "page";
 // 先頭のページ ID を保存しているファイル名
 const HEAD_FILENAME: &str = "HEAD";
 
+fn get_page_by_id(directory: &str, id: &str) -> Result<Page, String> {
+    // ページファイルのパスを取得
+    let filepath = Path::new(directory).join(PAGES_DIR).join(format!("{}.{}", id, PAGE_EXTENSION));
+    let filepath = filepath.as_path();
+
+    // ページファイルを読み込み
+    let mut file = match fs::File::open(filepath) {
+        Ok(file) => file,
+        Err(err) => return Err(format!("Unable to open page file `{}`: {}", filepath.to_string_lossy(), err)),
+    };
+
+    let mut contents = String::new();
+    if let Err(err) = file.read_to_string(&mut contents) {
+        return Err(format!("Unable to read page file `{}`: {}", filepath.to_string_lossy(), err));
+    }
+
+    // ページファイルの文字列をパース
+    let page = match Page::from_str(&contents, id) {
+        Ok(page) => page,
+        Err(err) => return Err(format!("{}", err)),
+    };
+
+    Ok(page)
+}
+
 pub fn list(directory: &str, _no_color: bool) -> Result<(), String> {
     // 先頭のページ ID を保存しているファイルパスを取得
     let head_filepath = Path::new(directory).join(HEAD_FILENAME);
@@ -29,26 +54,7 @@ pub fn list(directory: &str, _no_color: bool) -> Result<(), String> {
 
     let mut prev_id = head_id;
     loop {
-        // ページファイルのパスを取得
-        let diary_filepath = Path::new(directory).join(PAGES_DIR).join(format!("{}.{}", prev_id, PAGE_EXTENSION));
-        let diary_filepath = diary_filepath.as_path();
-
-        // ページファイルを読み込み
-        let mut file = match fs::File::open(diary_filepath) {
-            Ok(file) => file,
-            Err(err) => return Err(format!("Unable to open page file `{}`: {}", diary_filepath.to_string_lossy(), err)),
-        };
-
-        let mut contents = String::new();
-        if let Err(err) = file.read_to_string(&mut contents) {
-            return Err(format!("Unable to read page file `{}`: {}", diary_filepath.to_string_lossy(), err));
-        }
-
-        // ページファイルの文字列をパース
-        let page = match Page::from_str(&contents, &prev_id) {
-            Ok(page) => page,
-            Err(err) => return Err(format!("{}", err)),
-        };
+        let page = get_page_by_id(directory, &prev_id)?;
 
         // ページの情報を出力
         println!("{}: {}", page.id, page.header.title);
